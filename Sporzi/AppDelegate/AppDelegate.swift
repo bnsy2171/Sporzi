cac490d339d0a529707574b3a9948efb61e369a1
Hakan <support@lolmess.com>
2016-11-28 11:54:45 +0530

    Initial Commit

--- Sporzi/AppDelegate/AppDelegate.swift
+++ Sporzi/AppDelegate/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
    IQKeyboardManager.sharedManager().enable = true
-        print("hello")
+        print("hello1")
         // Override point for customization after application launch.
         return true
     }
